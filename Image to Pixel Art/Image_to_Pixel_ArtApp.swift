d92264909fd26017ff015e3f3c4536c439ebee5a
TopScrech <sergei_saliukov@icloud.com>
2026-03-11 00:46:24 +0100

    Initial Commit

---
+++ Image to Pixel Art/Image_to_Pixel_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_to_Pixel_ArtApp.swift
+//  Image to Pixel Art
+//
+//  Created by Sergei Saliukov on 11/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct Image_to_Pixel_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
